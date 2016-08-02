5fc1fa8b503a09eb71618b4dc0c54e5bce700d01
Demo User <demouser@MacBook-Pro-3.local>
2016-08-02 17:25:11 -0400

    got form info display and storage to work for all text based input

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ import webapp2
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class Recipe(ndb.Model):
-    author = ndb.StringProperty(required=True)
-    recipe_name = ndb.StringProperty(required=True)
-    level = ndb.StringProperty(required=True)
-
 class NewRecipeHandler(webapp2.RequestHandler):
 
     def get(self):
         template = jinja_environment.get_template('templates/new_recipe.html')
+
         self.response.write(template.render())
 
 class ResultsHandler(webapp2.RequestHandler):
@@ -23,20 +19,37 @@ class ResultsHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('templates/results.html')
         author_value=self.request.get('author')
         name_value=self.request.get('name')
-        level_value=self.request.get('level')
-
+        level_value=(self.request.get('level'))
+        new_ingredients_value=self.request.get('new_ingredients')
+        time_value=self.request.get('time')
+        steps_value=self.request.get('steps')
+        notes_value=self.request.get('notes')
+        
         new_recipe = {
+
         'author_answer': author_value,
         'name_answer': name_value,
-        'level_answer': level_value
+        'level_answer': level_value,
+        'new_ingredients_answer': new_ingredients_value,
+        'time_answer': time_value,
+        'steps_answer': steps_value,
+        'notes_answer': notes_value
         }
-
-        recipe_record = Recipe(author=author_value, recipe_name=name_value, level=level_value)
+        recipe_record = Recipe(author=author_value, recipe_name=name_value, level=level_value, new_ingredients=new_ingredients_value, time=time_value, steps=steps_value, notes=notes_value)
         recipe_key = recipe_record.put()
 
         self.response.write(template.render(new_recipe))
 
+class Recipe(ndb.Model):
+    author = ndb.StringProperty(required=True)
+    recipe_name = ndb.StringProperty(required=True)
+    level = ndb.StringProperty(required=True)
+    new_ingredients = ndb.StringProperty(required=True)
+    time = ndb.StringProperty(required=True)
+    steps = ndb.StringProperty(required=True)
+    notes = ndb.StringProperty(required=True)
+
 app = webapp2.WSGIApplication([
   ('/', NewRecipeHandler),
-  ('/results', ResultsHandler)
+  ('/results', ResultsHandler),
 ], debug=True)
